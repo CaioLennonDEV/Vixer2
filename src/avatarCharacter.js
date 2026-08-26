c2c81db72e6142d4cb9a14d6c5523eccb2cccb55
Caio Lennon <caiolennon09@gmail.com>
2026-08-26 01:29:45 -0300

    feat: implement AvatarCharacter class for chroma-key video rendering and state management

--- src/avatarCharacter.js
+++ src/avatarCharacter.js
@@ -6,7 +6,7 @@
  */
 
 const idleVideo1Url = './Character_waving_and_blinking_eyes_202608252332.mp4';
-const idleVideo2Url = './Character_holding_notebook_with_…_202608260005.mp4';
+const idleVideo2Url = './Character_holding_notebook_202608260005.mp4';
 const thinkingVideoUrl = './Woman_resting_hand_on_chin_202608252343.mp4';
 
 export class AvatarCharacter {
